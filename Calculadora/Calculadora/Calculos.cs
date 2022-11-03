07866311e6a986e02d97bcc08ed450560225feba
Franco Minati <francominati2@gmail.com>
2022-11-03 01:45:02 -0300

    falta ver posibles casos de fallo y fixear cuentas que empiezan con un negativo

---
+++ Calculadora/Calculadora/Calculos.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+public class Calc
+{
+    public double Solve(string equation)
+    {
+        // Remove all spaces
+        equation = Regex.Replace(equation, @"\s+", "");
+
+        Operacion operation = new Operacion();
+        operation.Parse(equation);
+
+        double result = operation.Resolver();
+
+        return result;
+    }
+}
+
+public class Operacion
+{
+    public Operacion NumeroIzquierdo { get; set; }
+    public string Operador { get; set; }
+    public Operacion NumeroDerecho { get; set; }
+
+    private Regex sumaResta = new Regex("[+-]", RegexOptions.RightToLeft);
+    private Regex multiplicacionDivision = new Regex("[*/]", RegexOptions.RightToLeft);
+
+    public void Parse(string equation)
+    {
+        var operatorLocation = sumaResta.Match(equation);
+        if (!operatorLocation.Success)
+        {
+            operatorLocation = multiplicacionDivision.Match(equation);
+        }
+
+        if (operatorLocation.Success)
+        {
+            Operador = operatorLocation.Value;
+
+            NumeroIzquierdo = new Operacion();
+            NumeroIzquierdo.Parse(equation.Substring(0, operatorLocation.Index));
+
+            NumeroDerecho = new Operacion();
+            NumeroDerecho.Parse(equation.Substring(operatorLocation.Index + 1));
+        }
+        else
+        {
+            Operador = "v";
+            result = double.Parse(equation);
+        }
+    }
+
+    private double result;
+
+    public double Resolver()
+    {
+        switch (Operador)
+        {
+            case "v":
+                break;
+            case "+":
+                result = NumeroIzquierdo.Resolver() + NumeroDerecho.Resolver();
+                break;
+            case "-":
+                result = NumeroIzquierdo.Resolver() - NumeroDerecho.Resolver();
+                break;
+            case "*":
+                result = NumeroIzquierdo.Resolver() * NumeroDerecho.Resolver();
+                break;
+            case "/":
+                result = NumeroIzquierdo.Resolver() / NumeroDerecho.Resolver();
+                break;
+            default:
+                throw new Exception("Call Parse first.");
+        }
+
+        return result;
+    }
+}
--- Calculadora/Calculadora/Form1.cs
+++ Calculadora/Calculadora/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,7 +19,8 @@ namespace Calculadora
         Regex operandos = new Regex(@"(-?[1-9]\d*)");
         Regex operadores = new Regex(@"[\+\-\/\*\^\%]");
         Regex operacion_basica = new Regex(@"([\d+(\.\d+)]*[\+\-\/\*\^\%]+[\d+(\.\d+)]*)");
-        List<int> valores = new List<int>();
+        Regex operacion_unitaria = new Regex(@"(\({1}[\d\+\-\/\*\^\%]+[\d+]*\){1})");
+        List<String> partes_de_ecuacion = new List<String>();
         public Form1()
         {
             InitializeComponent();
@@ -91,7 +93,8 @@ namespace Calculadora
             if (txtResultado.Text.Length > 0)
             {
                 string n_anterior = "" + txtResultado.Text[txtResultado.Text.Length - 1];
-                bool is_operator = is_number(n_anterior) ;
+                bool is_operator = operadores.IsMatch(n_anterior);
+                
                 if (is_operator || n_anterior.Equals("("))
                 {
                     txtResultado.Text += "(";
@@ -123,14 +126,43 @@ namespace Calculadora
         private void button_result_Click(object sender, EventArgs e)
         {
             String cuenta = txtResultado.Text;
-            /*Calc _calculator = new Calc();*/
+            List<double> resultados = new List<double>();
+           
+            Calc _calculator = new Calc();
+            String nuevoR;
             if (cuenta.Contains('(') || cuenta.Contains(')'))
             {
                 Console.WriteLine("Ecuacion con parentesis");
                 if (validarEcuacion(cuenta))
                 {
-                    /*double valor = _calculator.Solve(cuenta);*/
-                    Console.WriteLine("Caso parentesis valido");
+                    
+                    separar_en_partes(cuenta);
+                    foreach (String parte in partes_de_ecuacion)
+                    {
+                        Console.WriteLine(parte);
+                    }
+                        List<String> quita_parentesis = new List<String>();
+                    double resultado = 0;
+                    int i = 0;
+                        foreach (String parte in partes_de_ecuacion)
+                        {
+                        
+                        //Copio las partes para poder borrar los parentesis
+                        quita_parentesis.Add(parte);
+                        //Guardo los resultados para despues reemplazarlos
+                        resultados.Add(_calculator.Solve(remover_parentesis(parte)));
+                        //Hago el reemplazo en la cuenta
+                        cuenta = cuenta.Replace(parte, Convert.ToString(resultados[i]));
+                        Console.WriteLine("cuenta modificada: " + cuenta);
+                        i++;
+
+                        }
+                   
+                       
+                        resultado = _calculator.Solve(remover_parentesis(cuenta));
+                    
+                    
+                    Console.WriteLine("Resultado: " + resultado);
                 }
                 else
                 {
@@ -141,6 +173,8 @@ namespace Calculadora
             else
             {
                 Console.WriteLine("Ecuacion sin parentesis");
+                double resultado = _calculator.Solve(cuenta);
+                Console.WriteLine("Resultado: " + resultado);
             }
         }
         private bool validarEcuacion(String cuenta)
@@ -162,5 +196,55 @@ namespace Calculadora
             }
             return contador_pd == contador_pi ? true : false;
         }
+        private int contar_parentesis(String cuenta)
+        {
+            char p_i = '(';
+            char p_d = ')';
+            
+            int contador = 0;
+            
+            foreach (char p in cuenta)
+            {
+                if (p.Equals(p_i)||p.Equals(p_d))
+                {
+                    contador++;
+                }
+            }
+            return contador;
+        }
+        private List<String> separar_en_partes(String cuenta)
+        {
+            // Limpio la lista por cada operacion nueva
+            partes_de_ecuacion.Clear();
+            // Si la operacion tiene parentesis al inicio y al final, los remuevo ya que da igual si los tiene
+            if (cuenta[0]=='(' && cuenta[cuenta.Length - 1] == ')' && contar_parentesis(cuenta)==2)
+            {
+                
+                partes_de_ecuacion.Add(cuenta);
+                return partes_de_ecuacion;
+            }
+            Regex n = new Regex(@"(\({2}[\d\+\-\/\*\^\%]+[\d+]*\){2})");
+            foreach(Match m in operacion_unitaria.Matches(cuenta))
+            {
+                partes_de_ecuacion.Add(Convert.ToString(m));
+               
+                
+            }
+            
+            return partes_de_ecuacion;
+            
+        }
+        private String remover_parentesis(String cuenta)
+        {
+            if (cuenta.Contains("("))
+            {
+                cuenta = cuenta.Remove(0, 1);
+                cuenta = cuenta.Remove(cuenta.Length - 1, 1);
+            }
+            return cuenta;
+
+        }
+       
+
     }
 }
